f742235ec8485e14d98c1ac42b3a9420e5ab6ee4
StuartBacklund <stuart.backlund@gmail.com>
2017-03-09 18:33:38 +0200

    Add project files.

---
+++ LabTest.Presentation.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LabTest.Presentation.Web.MvcApplication" Language="C#" %>
---
+++ LabTest.Presentation.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LabTest.Presentation.Web.Startup))]
+namespace LabTest.Presentation.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
